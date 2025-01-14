97025d7367c6fa2d19db824cb0dea941eee2cbc2
Martin M. <meszaros.martin986@gmail.com>
2025-01-14 20:05:57 +0100

    TCP update
    Rewrote the entire file to support TCP for the best reliability

--- TrackPro_ESP_WebSocket/TrackPro_ESP_WebSocket.ino
+++ TrackPro_ESP_WebSocket/TrackPro_ESP_WebSocket.ino
@@ -1,61 +1,83 @@
 #include <ESP8266WiFi.h>
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
-#include <WebSocketsServer.h>
 
 #define GPSBaud 9600
 
 // GPS and SoftwareSerial objects
 TinyGPSPlus gps;
-SoftwareSerial gpsSerial(4, 5); // RX, TX
+SoftwareSerial gpsSerial(4, 5);  // RX, TX
 
 // Wi-Fi credentials for AP mode
 const char *ssid = "ESP8266";
 const char *password = "yourpasswordhere";
 
-// WebSocket server on port 81
-WebSocketsServer webSocket(81);
+// TCP server on port 4210
+WiFiServer tcpServer(4210);
 
 // Variables to store GPS data
 String lastGpsData = "";
-unsigned long lastUpdateTime = 0; // For timing updates
+unsigned long lastUpdateTime = 0;  // For timing updates
+bool gpsConnected = false;         // Flag for GPS connection
 
 void setup() {
   Serial.begin(115200);
   gpsSerial.begin(GPSBaud);
 
   // Configure ESP8266 as an Access Point
+  IPAddress local_IP(192, 168, 4, 1);
+  IPAddress gateway(192, 168, 4, 1);
+  IPAddress subnet(255, 255, 255, 0);
+
+  if (!WiFi.softAPConfig(local_IP, gateway, subnet)) {
+    Serial.println("Failed to configure AP IP address!");
+    return;
+  }
+
   WiFi.softAP(ssid, password);
   Serial.println("Access Point started");
   Serial.print("AP IP Address: ");
-  Serial.println(WiFi.softAPIP());
+  Serial.println(WiFi.softAPIP());  // Should display 192.168.4.1
 
-  // Start WebSocket server
-  webSocket.begin();
-  webSocket.onEvent(webSocketEvent);
-  Serial.println("WebSocket server started");
+  // Start TCP server
+  tcpServer.begin();
+  Serial.println("TCP server started on 192.168.4.1:4210");
 }
 
 void loop() {
-  // Process GPS data
-  while (gpsSerial.available() > 0) {
-    gps.encode(gpsSerial.read());
-    if (gps.location.isUpdated()) {
-      sendGpsUpdate();
-    }
-  }
+  // Check if any client is connected
+  WiFiClient client = tcpServer.available();
+  if (client) {
+    Serial.println("Client connected");
+    while (client.connected()) {
+      // Process GPS data
+      gpsConnected = false;
+      while (gpsSerial.available() > 0) {
+        gps.encode(gpsSerial.read());
+        gpsConnected = true;
+        if (gps.location.isUpdated()) {
+          sendGpsUpdate(client);
+        }
+      }
 
-  // Handle WebSocket events
-  webSocket.loop();
+      // Send dummy data if GPS is not connected
+      if (!gpsConnected) {
+        sendDummyData(client);
+      }
+    }
+    // Client disconnected
+    Serial.println("Client disconnected");
+    client.stop();
+  }
 }
 
-void sendGpsUpdate() {
+void sendGpsUpdate(WiFiClient &client) {
   String currentGpsData = createGpsJson();
 
   // Avoid sending the same data repeatedly
   if (currentGpsData != lastGpsData) {
     lastGpsData = currentGpsData;
-    webSocket.broadcastTXT(currentGpsData); // Send to all connected clients
+    client.println(currentGpsData);  // Send to the connected client
     Serial.println("GPS Update Sent: " + currentGpsData);
   }
 }
@@ -68,25 +90,25 @@ String createGpsJson() {
   json += "\"altitude\": " + String(gps.altitude.isValid() ? gps.altitude.meters() : 0) + ",";
   json += "\"speed\": " + String(gps.speed.isValid() ? gps.speed.kmph() : 0) + ",";
   json += "\"satellites\": " + String(gps.satellites.isValid() ? gps.satellites.value() : 0) + ",";
-  json += "\"timestamp\": \"" + String(gps.time.isValid() ? gps.time.hour() : 0) + ":" +
-          String(gps.time.minute()) + ":" + String(gps.time.second()) + "\"";
+  json += "\"timestamp\": \"" + String(gps.time.isValid() ? gps.time.hour() : 0) + ":" + String(gps.time.minute()) + ":" + String(gps.time.second()) + "\"";
   json += "}";
   return json;
 }
 
-void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length) {
-  switch (type) {
-    case WStype_DISCONNECTED:
-      Serial.printf("Client #%u disconnected\n", num);
-      break;
-    case WStype_CONNECTED:
-      Serial.printf("Client #%u connected\n", num);
-      webSocket.sendTXT(num, lastGpsData); // Send the last known GPS data on connection
-      break;
-    case WStype_TEXT:
-      Serial.printf("Received from client #%u: %s\n", num, payload);
-      break;
-    default:
-      break;
+void sendDummyData(WiFiClient &client) {
+  String dummyData = "{";
+  dummyData += "\"latitude\": " + String(random(-900000, 900000) / 10000.0, 6) + ",";
+  dummyData += "\"longitude\": " + String(random(-1800000, 1800000) / 10000.0, 6) + ",";
+  dummyData += "\"altitude\": " + String(random(0, 5000)) + ",";
+  dummyData += "\"speed\": " + String(random(0, 120)) + ",";
+  dummyData += "\"satellites\": " + String(random(1, 12)) + ",";
+  dummyData += "\"timestamp\": \"" + String(random(0, 24)) + ":" + String(random(0, 60)) + ":" + String(random(0, 60)) + "\"";
+  dummyData += "}";
+  delay(500);
+
+  if (dummyData != lastGpsData) {
+    lastGpsData = dummyData;
+    client.println(dummyData);  // Send to the connected client
+    Serial.println("Dummy Data Sent: " + dummyData);
   }
 }
